6d381794a86410f45de48fc299c7874e570b9191
Dmitry Astankov <astankov.dmitry@gmail.com>
2020-01-30 00:43:53 +0300

    BeautifulSoup is needed no more, Selenium was implemented instead.

--- sber-parser.py
+++ sber-parser.py
@@ -1,42 +1,48 @@
 import camelot
-import urllib
+from selenium import webdriver
+import urllib3
+import certifi
 import requests
 
-from bs4 import BeautifulSoup, SoupStrainer
-import requests
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
-#url = "https://www.python.org/downloads/"
 url = "https://www.sberbank-am.ru/disclosure/fund/etf-sp-500/"
 
-page = requests.get(url, verify=False)
-print(page.status_code)
+# Указываем полный путь к geckodriver.exe на ПК:
+#driver = webdriver.Firefox('D:\\Work\\geckodriver.exe')
+driver = webdriver.Firefox(executable_path="D:\\Work\\geckodriver.exe")
+driver.get(url)
 
-soup = BeautifulSoup(page.text, "html.parser")
+driver.find_element_by_link_text("Отчетность за 2019 год").click()
 
-links = soup.find_all('a', class_='', text="Отчетность за 2019 год")
-print(links)
+href = driver.find_element_by_partial_link_text("Отчет о приросте").get_attribute("href")
+print(href)
 
-#for i in range(len(links))
-#    if links[i].find()
+import urllib.request
+import shutil
 
-#for link in soup.find_all('a', class_='file'):
-#    print(link.get('href'))
+# Download the file from `url` and save it locally under `file_name`:
+with requests.get(href, verify=False) as response, open("temp", 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
 
-# Downloading the file:
+#driver.quit()
+
+tables = camelot.read_pdf(out_file)
+
+# Link to the source page:
 # https://www.sberbank-am.ru/disclosure/fund/etf-sp-500/
 # Прирост:
 # https://www.sberbank-am.ru/upload/iblock/287/287c6c9433d1ed70b7284b7dab7475e8.pdf
 # Стоимость:
 # https://www.sberbank-am.ru/upload/iblock/728/7280ae4e0f1938f4539ab4211fbaed73.pdf
-#with urllib.request.urlopen('https://www.sberbank-am.ru/disclosure/fund/etf-sp-500/') as response:
-#   html = response.read()
 
-# Mandatory (read the file):
+# MANDATORY
+# Read the file:
 #tables = camelot.read_pdf('D://Work//stoimost.pdf', process_background=False, pages='18', line_scale=45)
 
 # See some metrics about read tables:
-#for x in tables:
-#    print (x.parsing_report)
+for x in tables:
+    print (x.parsing_report)
 
 # Visual debugging:
 #plt = camelot.plot(tables[1], kind='text')
@@ -46,6 +52,7 @@ print(links)
 # Wait for input when seeing the plot:
 #input()
 
-# Mandatory (writing the tables):
+# MANDATORY
+# Writing the tables:
 #tables.export('D://Work//test.html', f='html', compress=False)
 
