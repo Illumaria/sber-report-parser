43b96cc3a757bebcc4ad52ed53c799001f50e3d6
Dmitry Astankov <astankov.dmitry@gmail.com>
2020-01-30 23:46:09 +0300

    Fixed SSL certification error

--- sber-parser.py
+++ sber-parser.py
@@ -1,58 +1,48 @@
+import ssl
 import camelot
+import urllib.request as urlrq
 from selenium import webdriver
-import urllib3
-import certifi
-import requests
 
-http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+ssl._create_default_https_context = ssl._create_unverified_context
 
 url = "https://www.sberbank-am.ru/disclosure/fund/etf-sp-500/"
 
-# Указываем полный путь к geckodriver.exe на ПК:
-#driver = webdriver.Firefox('D:\\Work\\geckodriver.exe')
-driver = webdriver.Firefox(executable_path="D:\\Work\\geckodriver.exe")
+# Set the full path to the geckodriver.exe:
+driver = webdriver.Firefox(executable_path='D:\\Work\\geckodriver.exe')
 driver.get(url)
 
 driver.find_element_by_link_text("Отчетность за 2019 год").click()
 
-href = driver.find_element_by_partial_link_text("Отчет о приросте").get_attribute("href")
-print(href)
+prop_reval_report = driver.find_element_by_partial_link_text("Отчет о приросте").get_attribute("href")
+print("Link to the property revaluation report: {0}".format(prop_reval_report))
 
-import urllib.request
-import shutil
+nav_report = driver.find_element_by_partial_link_text("Справка о стоимости").get_attribute("href")
+print("Link to the net asset value report: {0}".format(nav_report), end='\n')
 
-# Download the file from `url` and save it locally under `file_name`:
-with requests.get(href, verify=False) as response, open("temp", 'wb') as out_file:
-    shutil.copyfileobj(response, out_file)
+driver.quit()
 
-#driver.quit()
-
-tables = camelot.read_pdf(out_file)
-
-# Link to the source page:
-# https://www.sberbank-am.ru/disclosure/fund/etf-sp-500/
-# Прирост:
-# https://www.sberbank-am.ru/upload/iblock/287/287c6c9433d1ed70b7284b7dab7475e8.pdf
-# Стоимость:
-# https://www.sberbank-am.ru/upload/iblock/728/7280ae4e0f1938f4539ab4211fbaed73.pdf
-
-# MANDATORY
 # Read the file:
-#tables = camelot.read_pdf('D://Work//stoimost.pdf', process_background=False, pages='18', line_scale=45)
+prop_reval_tables = camelot.read_pdf(prop_reval_report, process_background=False, pages='1-end', line_scale=45)
 
-# See some metrics about read tables:
-for x in tables:
-    print (x.parsing_report)
+# Print parsing statictics:
+for x in prop_reval_tables:
+    print("{0}: {1}".format(x, x.parsing_report))
 
-# Visual debugging:
-#plt = camelot.plot(tables[1], kind='text')
-#plt = camelot.plot(tables[1], kind='joint')
+# Read the file:
+nav_tables = camelot.read_pdf(nav_report, process_background=False, pages='1-end', line_scale=45)
+
+# Print parsing statictics:
+for x in nav_tables:
+    print("{0}: {1}".format(x, x.parsing_report))
+
+# Visual debugging (if needed):
+#plt = camelot.plot(prop_reval_tables[1], kind='text')
+#plt = camelot.plot(prop_reval_tables[1], kind='joint')
 #plt.show()
-
-# Wait for input when seeing the plot:
+# Wait for input when using the plot or the window will close automatically:
 #input()
 
-# MANDATORY
-# Writing the tables:
-#tables.export('D://Work//test.html', f='html', compress=False)
+# Export the tables:
+prop_reval_tables.export('D://Work//prop_reval.html', f='html', compress=False)
+nav_tables.export('D://Work//nav.html', f='html', compress=False)
 
